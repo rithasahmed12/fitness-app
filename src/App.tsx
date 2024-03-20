9b451e33cfda75e24c6222a19821e90242b50b0d
rithasahmed12 <ahmedrithas48@gmail.com>
2024-03-20 21:00:50 +0530

    home page completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import Navbar from '@/scenes/navbar';
+import Home from '@/scenes/home';
 import { useEffect, useState } from 'react';
 import { SelectedPage } from './shared/type';
+
 
 
 function App() {
@@ -30,6 +32,9 @@ function App() {
         <Navbar
         isTopOfPage={isTopOfPage}
         selectedPage={selectedPage}
+        setSelectedPage={setSelectedPage}
+        />
+        <Home 
         setSelectedPage={setSelectedPage}
         />
 
