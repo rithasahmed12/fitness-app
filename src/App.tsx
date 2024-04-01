62cd71af8a34cf544fff797a782602d25d4dd377
rithasahmed12 <ahmedrithas48@gmail.com>
2024-04-01 07:49:46 +0530

    benefit page completed

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import Navbar from '@/scenes/navbar';
 import Home from '@/scenes/home';
+import Benefits from '@/scenes/benefits'
 import { useEffect, useState } from 'react';
 import { SelectedPage } from './shared/type';
 
@@ -35,6 +36,9 @@ function App() {
         setSelectedPage={setSelectedPage}
         />
         <Home 
+        setSelectedPage={setSelectedPage}
+        />
+        <Benefits 
         setSelectedPage={setSelectedPage}
         />
 
